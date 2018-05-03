d2ae06534caee2eb829ee8c878ecb1fef15a2bef
mbannert <matthias.bannert@gmail.com>
2018-05-03 16:09:10 +0200

    basic scaffold of the gui

---
+++ inst/app/ui.R
@@ -0,0 +1,35 @@
+# OVERVIEW ##################
+source("tabs.R")
+# DOWNLOAD ##################
+
+# PAGE LAYOUT ##########
+dashboardPage(header = 
+                dashboardHeader(title = "tstools viewr",
+                                titleWidth = 280),
+              dashboardSidebar(disable = TRUE),
+              body = dashboardBody(
+                tags$head(
+                  tags$link(rel = "stylesheet",
+                            type = "text/css", href = "custom.css")),
+                fluidRow(
+                  box(title = "Graph",
+                      width = 10, solidHeader = T,
+                      status = "primary"),
+                  box(title = "info",
+                      width = 2, solidHeader = T,
+                      status = "primary")
+                ),
+                fluidRow(
+                  tabBox(
+                    # The id lets us use input$tabset1 on the server to find the current tab
+                    id = "tabset1", height = "250px",
+                    tabPanel("Data", "reading data"),
+                    tabPanel("Select", "Select time series"),
+                    tabPanel("Plot properties", "all the switches and buttons of tsplot"),
+                    tabPanel("Export image", "png, pdf and so on"),
+                    tabPanel("Export data", "long format wide format and so on"),
+                    width = 12
+                  )
+                )
+              )
+)
